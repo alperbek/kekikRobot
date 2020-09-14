ffe34236b49526afa783b67dbe77476959c6321b
keyiflerolsun <keyiflerolsun@gmail.com>
2020-09-14 09:55:19 +0300

    v1 Stable

---
+++ roBot/Eklentiler/akaryakit.py
@@ -0,0 +1,54 @@
+# Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
+
+from pyrogram import Client, filters
+import asyncio
+import requests
+
+from roBot._edevat import logYolla
+
+@Client.on_message(filters.command(['akaryakit'], ['!','.','/']))
+async def akaryakit(client, message):
+    await logYolla(client, message)
+    # < Başlangıç    
+    cevaplanan_mesaj    = message.reply_to_message
+    if cevaplanan_mesaj is None:
+        yanitlanacak_mesaj  = message.message_id
+    else:
+        yanitlanacak_mesaj = cevaplanan_mesaj.message_id
+    
+    ilk_mesaj = await message.reply("__Bekleyin..__",
+        reply_to_message_id         = yanitlanacak_mesaj,
+        disable_web_page_preview    = True,
+        parse_mode                  = "Markdown"
+    )
+    #------------------------------------------------------------- Başlangıç >
+
+    istek = requests.get(f"https://kolektifapi.herokuapp.com/akaryakit")
+
+    api_yaniti = istek.json()
+
+    mesaj = "__Güncel Akaryakıt Verileri;__\n\n"
+    for yanit in api_yaniti:
+        mesaj += f"`{yanit['fiyati'].split(' ')[0]} ₺`\t**{yanit['turu'].split(' -- ')[0]}**\n"
+
+    try:
+        await ilk_mesaj.edit(mesaj)
+    except Exception as hata:
+        await ilk_mesaj.edit(f"**Uuppss:**\n\n`{hata}`")
+
+
+from roBot import DESTEK_KOMUT
+from pathlib import Path
+
+DESTEK_KOMUT.update({
+    Path(__file__).stem : {
+        "komut"        : "akaryakit",
+        "aciklama"     : "kolektifapi.herokuapp.com kullanarak güncel akaryakıt fiyatlarını verir..",
+        "parametreler" : [
+            None
+            ],
+        "ornekler"     : [
+            ".akaryakit"
+            ]
+    }
+})
